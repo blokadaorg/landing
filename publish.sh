9b27b13c79a7cf9f91b3350213d93219f285a87c
kar <hi@kar.gs>
2020-11-20 09:14:14 +0100

    modify publish.sh to tag version

--- publish.sh
+++ publish.sh
@@ -13,13 +13,21 @@ cd ..
 yarn build
 cp -r dist/* landing-github-pages/
 
+echo $commit > landing-github-pages/version.txt
+
 echo $commit
 cd landing-github-pages
+git add .
 git commit -am "$commit"
+git tag $commit
 git push
+git push --tags
 
 cd ..
 git commit -am "$commit"
+git tag $commit
 git push
+git push --tags
+
 
 echo "Done"
